3e1a5b6a7d36d186762212fe826c83d55da2b3ea
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-12-27 19:42:45 +0300

    styles done

--- all-ideas/preload.js
+++ all-ideas/preload.js
@@ -14,4 +14,18 @@ function preloadAudio (filename) {
 
 }
 
+const audioArray = ['audio1.mp3', 'audio2.mp3', 'audio3.mp3'];
+let currentIndex = 0;
+
+function preloadNextAudio() {
+    // Create a new Audio object
+    const audio = new Audio();
+
+    // Set the src to the next audio in the array
+    audio.src = audioArray[currentIndex + 1];
+
+    // Start preloading the audio
+    audio.load();
+}
+
 'https://stackoverflow.com/questions/29723089/preloading-the-next-song-in-a-playlist-a-bit-before-the-current-one-ends'
